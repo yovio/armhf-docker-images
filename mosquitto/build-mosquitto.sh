93d51fc629a8535c0a4098ea10dddf034eb56b43
Yovi Oktofianus <yovio@hotmail.com>
2017-06-16 21:58:02 +0800

    updtes

--- mosquitto/build-mosquitto.sh
+++ mosquitto/build-mosquitto.sh
@@ -1,4 +1,4 @@
-MOSQUITTO_VER=1.4.12-r0
+MQT_VER=1.4.12-r0
 
 mkdir tmp
 pushd tmp &&
@@ -6,16 +6,16 @@ curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/
 tar xzf qemu-arm-static.tar.gz &&
 popd
 # build image
-docker build -t yovio/armhf-mosquitto:build --build-arg MOSQUITTO_VER=$MOSQUITTO_VER .
+docker build -t yovio/armhf-mosquitto:build --build-arg MOSQUITTO_VER=$MQT_VER .
 # test image
 docker run yovio/armhf-mosquitto:build uname -a
 # push image
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
   TAG=$(grep "FROM " Dockerfile | sed 's/.*://')
-  docker tag yovio/armhf-mosquitto:build yovio/armhf-mosquitto:$MOSQUITTO_VER
+  docker tag yovio/armhf-mosquitto:build yovio/armhf-mosquitto:$MQT_VER
   docker tag yovio/armhf-mosquitto:build yovio/armhf-mosquitto:latest
-  docker push yovio/armhf-mosquitto:$MOSQUITTO_VER
+  docker push yovio/armhf-mosquitto:$MQT_VER
   docker push yovio/armhf-mosquitto:latest
 fi
 rm -Rf tmp
